b15013353e98a585c81c517c9c3caca1ea75b121
Martina <martina.pozzerle@libero.it>
2021-03-13 12:04:01 +0100

    bg images + bg body

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,38 @@
           class="search-bar" 
           placeholder="Search..."
           v-model="query"
+          
           @keypress="fetchWeather"
         />
       </div>
 
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+      <div class="weather-container animate__animated animate__fadeInDown" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-          <div class="weather">{{ weather.weather[0].main }}</div>
-        </div>
+          <div class="card-temp">
+            <div class="inner">
+              <div class="front">{{ Math.round(weather.main.temp) }}°c</div>
+                <div class="back">
+                  <div class="min">Min: {{ Math.round(weather.main.temp_min) }}°c</div>
+                  <div class="max">Max: {{ Math.round(weather.main.temp_max) }}°c</div>
+               </div>
+              </div>
+            </div>
+              <div class="weather">{{ weather.weather[0].main }}</div>
+            </div>
+            
+              <div class="description">
+                <h3>Vincent Van Gogh</h3>
+                <p>{{descriptionBuilder()}}</p>
+            </div>
+        
       </div>
+      
+      
     </main>
   </div>
 </template>
@@ -57,30 +74,45 @@ export default {
       console.log(this.big);
     },
     dateBuilder () {
-      console.log(this.weather);
-      let d = new Date();
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-      return `${day} ${date} ${month} ${year}`;
+      let current_datetime = new Date();
+      let weekday =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+
+      let formatted_date = weekday[current_datetime.getDay()] + ", " + current_datetime.getDate() + " " + months[current_datetime.getMonth()] + " " + current_datetime.getFullYear();
+      return formatted_date;
     },
-    
+    descriptionBuilder(){
+      if(this.big =="clear"){
+        console.log(this.big);
+        
+        return 'Olive Trees with Yellow Sky and Sun - 1889' ;
+      }else if(this.big == 'snow'){
+        return 'Landscape with Snow - 1888';
+      }else if(this.big == "clouds"){
+        return 'Wheatfields Under Thunderclouds - 1890';
+      }else if(this.big =="rain"){
+         return 'Landscape in the Rain - 1890';
+        }else{
+        return '';
+      }
     }
+    
   }
+}
 
 </script>
 
 <style>
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Permanent Marker', cursive;
 }
 body {
   font-family: 'montserrat', sans-serif;
+  background-color: #a0653b;
 }
 
 #app{
@@ -88,17 +120,23 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+  width:95%;
+  z-index:100;
+  margin:20px auto;
   
 }
 #app.clear {
   background-image: url('./assets/sun-bg.jpg');
 }
 #app.clouds{
+  background-image: url('./assets/clouds-bg.jpg');
+}
+#app.rain{
   background-image: url('./assets/rain-bg.jpg');
 }
 main {
-  min-height: 100vh;
-  padding: 25px;
+  min-height: 95vh;
+  padding: 20px;
   background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
 .search-box {
@@ -107,24 +145,23 @@ main {
 }
 .search-box .search-bar {
   display: block;
-  width: 100%;
+  width: 10%;
   padding: 15px;
-  
+  outline: none;
   color: #313131;
   font-size: 20px;
-  appearance: none;
+  margin:0 auto;
   border:none;
-  outline: none;
   background: none;
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.5);
-  border-radius: 0px 16px 0px 16px;
-  transition: 0.4s;
+  transition: 1s;
 }
 .search-box .search-bar:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 16px 0px 16px 0px;
+  border-radius: 20px;
+  width:50%;
 }
 .location-box .location {
   color: #FFF;
@@ -132,6 +169,7 @@ main {
   font-weight: 500;
   text-align: center;
   text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  
 }
 .location-box .date {
   color: #FFF;
@@ -139,27 +177,84 @@ main {
   font-weight: 300;
   font-style: italic;
   text-align: center;
+  margin:30px 0;
 }
-.weather-box {
+.weather-container {
   text-align: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 }
-.weather-box .temp {
-  display: inline-block;
-  padding: 10px 25px;
+.weather-container .front {
   color: #FFF;
   font-size: 102px;
   font-weight: 900;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color:rgba(255, 255, 255, 0.25);
+  background-color:transparent;
   border-radius: 16px;
-  margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  height:200px;
+  
 }
-.weather-box .weather {
+.min, .max{
+  color: white;
+}
+
+.card-temp{
+  background-color: transparent;
+  width: 300px;
+  height: 200px;
+  perspective: 1000px; 
+}
+.inner{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+.card-temp:hover .inner{
+  transform: rotateY(180deg);
+
+}
+.card-temp:hover 
+.front, .back{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  
+}
+.back{
+  transform: rotateY(180deg);
+  
+  font-size: 50px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-image:linear-gradient(darkblue, red);
+  border-radius: 16px;
+  height:200px;
+  
+}
+.weather-container .weather {
   color: #FFF;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  margin:30px 0;
+}
+.description{
+  color:white;
+  font-size:20px;
+  font-style: italic;
+  align-self: flex-end;
+ 
+}
+.description:hover main{
+  background-image: linear-gradient(to bottom,  rgba(255, 255, 255, 7), rgba(255, 255, 255, 7));
 }
 </style>
